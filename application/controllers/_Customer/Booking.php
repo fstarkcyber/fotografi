5a5980b5720bbdb0172553ce833ea057ab4c168f
fstarkcyber <fstarkcyber@gmail.com>
2022-06-18 23:19:39 +0700

    Tambah Fotografer

--- application/controllers/_Customer/Booking.php
+++ application/controllers/_Customer/Booking.php
@@ -62,8 +62,7 @@ class Booking extends MY_Controller
 					$date = $tr->datetime_fix;
 				}
 
-				$status .= '<span class="badge badge-success">Validasi Berhasil</span>';
-				$status .= '<span class="badge badge-danger">Jadwal di ubah</span>';
+				$status = '<span class="badge badge-success">Validasi Berhasil</span><span class="badge badge-danger">Jadwal di ubah</span>';
 			}
 
 			$data[] = array(
